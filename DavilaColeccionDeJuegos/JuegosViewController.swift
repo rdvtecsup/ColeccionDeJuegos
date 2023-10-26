c059363bfc56c22f16772736f4eff40b2ae19883
Randy Blasco Davila Vargas <randy.davila@tecsup.edu.pe>
2023-10-26 09:50:16 -0500

    Integración de mostrar imágenes en tableView principal.

---
+++ DavilaColeccionDeJuegos/JuegosViewController.swift
@@ -0,0 +1,48 @@
+//
+//  JuegosViewController.swift
+//  DavilaColeccionDeJuegos
+//
+//  Created by Randy Dávila on 24/10/23.
+//
+
+import UIKit
+
+class JuegosViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
+    @IBOutlet weak var JuegoImageView: UIImageView!
+    
+    @IBOutlet weak var tituloTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    
+    @IBAction func fotosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true,
+                completion: nil
+        )
+    }
+    
+    @IBAction func agregarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let juego = Juego(context: context)
+        juego.titulo = tituloTextField.text
+        juego.imagen = JuegoImageView.image?.jpegData(compressionQuality: 0.50)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[UIImagePickerController.InfoKey : Any]){
+        let imagenSeleccionada = info[.originalImage] as? UIImage
+        JuegoImageView.image = imagenSeleccionada
+        imagePicker.dismiss(animated: true,completion: nil)
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        
+    }
+    
+
+}
